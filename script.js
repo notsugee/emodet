c319c280a4233894259e8494da435953ffe1415e
Sujith <p.sujith303@gmail.com>
2023-11-16 15:52:53 +0530

    added audio play functionality

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const canvasElement = document.getElementById("canvas");
 const webcam = new Webcam(webCamElement, "user", canvasElement);
 webcam.start();
 
+let currentAudio = null; // Variable to track the currently playing audio
+
 function dataURItoBlob(dataURI) {
     const byteString = atob(dataURI.split(',')[1]);
     const ab = new ArrayBuffer(byteString.length);
@@ -34,19 +36,19 @@ updateImage.addEventListener('click', function () {
 let getFile = document.getElementById("file");
 
 
-inputFile.addEventListener('change', function () {
-    selectedFile = inputFile.files[0];
-    if (selectedFile) {
-        profilePic.src = URL.createObjectURL(selectedFile);
-    }
-});
-
-
 function takeSnap() {
     const picture = webcam.snap();
     profilePic.src = picture;
     selectedFile = dataURItoBlob(picture);
-    
+
+    // Pause the current audio and reset the icon to play
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+        playIcon.src = 'images/play.png';
+        currentAudio = null;
+    }
+
     // Function to upload the image and get the predicted class
     uploadImage(selectedFile);
 }
@@ -82,6 +84,25 @@ function uploadImage(file) {
 
 function displayPredictedClass(predictedClass) {
     predictedClassElement.textContent = `predicted emotion: ${predictedClass}`;
+
+    // Pause the current audio if it is playing
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+        playIcon.src = 'images/play.png';
+    }
+
+    // Get the new audio element based on the predicted emotion
+    const audioElement = emotionAudioMap[predictedClass];
+
+    if (audioElement) {
+        // Play the new audio
+        audioElement.play();
+        playIcon.src = 'images/pause.png';
+
+        // Set the new audio as the current audio
+        currentAudio = audioElement;
+    }
 }
 
 
@@ -105,4 +126,67 @@ document.getElementById("snap-btn").addEventListener('click', function () {
     const selectedFile = dataURItoBlob(picture);
     // Function to upload the image and get the predicted class
     uploadImage(selectedFile);
+});
+
+const emotionAudioMap = {
+    'angry': new Audio('media/angry.mp3'),
+    'disgusted': new Audio('media/disgusted.mp3'),
+    'fearful': new Audio('media/fearful.mp3'),
+    'happy': new Audio('media/happy.mp3'),
+    'neutral': new Audio('media/neutral.mp3'),
+    'sad': new Audio('media/sad.mp3'),
+    'surprised': new Audio('media/surprised.mp3')
+};
+
+
+
+playIcon.addEventListener('click', function () {
+    console.log("Play icon clicked");
+    const predictedEmotion = predictedClassElement.textContent.split(': ')[1];
+    console.log("Predicted Emotion:", predictedEmotion);
+    const audioElement = emotionAudioMap[predictedEmotion];
+
+    console.log("Audio Element:", audioElement);
+    console.log("Audio Source:", audioElement ? audioElement.src : "N/A");
+    console.log("Emotion Audio Map:", emotionAudioMap);
+
+    if (audioElement) {
+        if (currentAudio && currentAudio !== audioElement) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0; // Reset the current audio to the beginning
+            playIcon.src = 'images/play.png';
+        }
+
+        audioElement.onended = function () {
+            playIcon.src = 'images/play.png';
+            currentAudio = null;
+        };
+
+        if (audioElement.paused) {
+            audioElement.play().then(() => {
+                playIcon.src = 'images/pause.png';
+                currentAudio = audioElement;
+            }).catch((error) => {
+                console.error('Error playing audio:', error);
+            });
+        } else {
+            audioElement.pause();
+            playIcon.src = 'images/play.png';
+            currentAudio = null;
+        }
+    }
+});
+
+inputFile.addEventListener('change', function () {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+    playIcon.src = 'images/play.png';
+    currentAudio = null;
+
+    selectedFile = inputFile.files[0];
+    if (selectedFile) {
+        profilePic.src = URL.createObjectURL(selectedFile);
+    }
 });
